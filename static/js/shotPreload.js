9d906fff5c1ef8bac425b27a9884982717376761
Fan Li <fanli@example.com>
2026-04-02 21:04:45 +0800

    chore: remove dead code and old files
    - Remove old Electron/Vue frontend (index.html, chat.html, renderer.js, vue_*.js)
    - Remove old frontend assets (static/css/, static/source/, static/fontawesome/, static/libs/)
    - Remove old Electron main files (main.js, start.js)
    - Remove build artifacts (__pycache__/)
    
    Total: ~1MB of dead code removed

--- static/js/shotPreload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  finishShot: (rect) => ipcRenderer.send('screenshot-selected', rect),
-  cancelShot:  () => ipcRenderer.send('screenshot-selected', null)
-})
